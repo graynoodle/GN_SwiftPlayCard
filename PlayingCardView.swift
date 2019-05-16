867df1a7f3eb51d689c6f99d0ce7a3dfb070238e
lolo2513 <graynoodle@qq.com>
2019-05-16 18:37:33 +0800

    Initial commit

---
+++ PlayingCardView.swift
@@ -0,0 +1,33 @@
+//
+//  PlayingCardView.swift
+//  GN_SwiftPlayCard
+//
+//  Created by graynoodle on 2019/5/16.
+//  Copyright © 2019 graynoodle. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+//        if let context = UIGraphicsGetCurrentContext() {
+//            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//            context.setLineWidth(5.0)
+//            UIColor.green.setFill()
+//            UIColor.red.setStroke()
+//            context.strokePath()
+//            context.fillPath()
+//        }
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
+        UIColor.green.setFill()
+        UIColor.red.setStroke()
+        path.stroke()
+        path.fill()
+    }
+    
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GN_SwiftPlayCard
+//
+//  Created by graynoodle on 2019/5/16.
+//  Copyright © 2019 graynoodle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var deck = PlayingCardDeck()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // CustomStringConvertible
+//        for _ in 1...10 {
+//            if let card = deck.draw() {
+//                print("\(card)")
+//            }
+//        }
+    }
+
+
+}
+
