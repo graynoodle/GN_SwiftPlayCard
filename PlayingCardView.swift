08d98d2921cd594bdd84c2a8feb8e5a9ba9874b3
lolo2513 <graynoodle@qq.com>
2019-05-17 16:13:42 +0800

    5.17

--- PlayingCardView.swift
+++ PlayingCardView.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class PlayingCardView: UIView {
 
+    var rank: Int = 5 { didSet { setNeedsDisplay(); setNeedsLayout()} }
+    var suit: String = "♥️" { didSet { setNeedsDisplay(); setNeedsLayout()} }
+    var isFaceUp: Bool = true { didSet { setNeedsDisplay(); setNeedsLayout()} }
+    private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+    
+        return NSAttributedString(string: string, attributes: [NSAttributedString.Key.paragraphStyle:paragraphStyle,.font:font])
+    }
+    
+    private var cornerString: NSAttributedString {
+        return centeredAttributedString(rankString+"\n"+suit, fontSize: cornerFontSize)
+    }
+    
     override func draw(_ rect: CGRect) {
         // Drawing code
 //        if let context = UIGraphicsGetCurrentContext() {
@@ -20,14 +36,51 @@ class PlayingCardView: UIView {
 //            context.strokePath()
 //            context.fillPath()
 //        }
-        let path = UIBezierPath()
-        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-        path.lineWidth = 5.0
-        UIColor.green.setFill()
-        UIColor.red.setStroke()
-        path.stroke()
-        path.fill()
+        
+//        let path = UIBezierPath()
+//        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//        path.lineWidth = 5.0
+//        UIColor.green.setFill()
+//        UIColor.red.setStroke()
+//        path.stroke()
+//        path.fill()
+        
+        
+        
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+        roundedRect.addClip()
+        UIColor.white.setFill()
+        roundedRect.fill()
+        
     }
     
 
+}
+
+extension PlayingCardView {
+    private struct SizeRatio {
+        static let cornerFontSizeToBoundsHeight: CGFloat = 0.005
+        static let cornerRadiusToBoundsHeight: CGFloat = 0.06
+        static let cornerOffsetToCornerRadius: CGFloat = 0.34
+        static let faceCardImageSizeToBoundsSize: CGFloat = 0.76
+    }
+    private var cornerRadius: CGFloat {
+        return bounds.size.height * SizeRatio.cornerRadiusToBoundsHeight
+    }
+    private var cornerOffset: CGFloat {
+        return cornerRadius * SizeRatio.cornerOffsetToCornerRadius
+    }
+    private var cornerFontSize: CGFloat {
+        return bounds.size.height * SizeRatio.cornerFontSizeToBoundsHeight
+    }
+    private var rankString: String {
+        switch rank {
+        case 1: return "A"
+        case 2...10: return String(rank)
+        case 11: return "J"
+        case 12: return "Q"
+        case 13: return "K"
+        default: return "?"
+        }
+    }
 }
